3477a87a80a87b254567f9db605c7331b17c34cf
AndriyNich <info@bembi.ua>
2022-12-18 19:35:28 +0200

    pre-finish2

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,27 +4,38 @@ const form = document.querySelector('form');
 
 const feedbackFormState = {};
 
-function loadDataFromStorage() {
-  const value = JSON.parse(localStorage.getItem('feedback-form-state'));
+function clearForm() {
   for (const elem of form.elements) {
     if (elem.nodeName === 'BUTTON') {
       continue;
     }
+
+    elem.value = '';
     feedbackFormState[elem.name] = '';
-    try {
+  }
+}
+
+function loadDataFromStorage() {
+  clearForm();
+
+  try {
+    const value = JSON.parse(localStorage.getItem('feedback-form-state'));
+    for (const elem of form.elements) {
       const valueElem = value[elem.name];
+
       if (valueElem) {
         elem.value = valueElem;
         feedbackFormState[elem.name] = valueElem;
       }
-    } catch {
-      elem.value = '';
     }
+  } catch {
+    localStorage.removeItem('feedback-form-state');
   }
 }
 
 function onFormInput(event) {
   feedbackFormState[event.target.name] = event.target.value;
+
   localStorage.setItem(
     'feedback-form-state',
     JSON.stringify(feedbackFormState)
@@ -33,10 +44,11 @@ function onFormInput(event) {
 
 function onFormSubmit(event) {
   event.preventDefault();
+
   console.log(feedbackFormState);
-  //   feedbackFormState = {};
+
   localStorage.removeItem('feedback-form-state');
-  loadDataFromStorage();
+  clearForm();
 }
 
 form.addEventListener('input', throttle(onFormInput, 500));
