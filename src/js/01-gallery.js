b9295dbaa0e7013fcfa48a1a390ef492d75d6c5f
AndriyNich <info@bembi.ua>
2022-12-18 14:05:25 +0200

    gallery-end

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,26 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const createGallery = galleryItems =>
+  galleryItems
+    .map(
+      ({ original, preview, description }) =>
+        `<li>
+          <a class="gallery__item" href="${original}">
+            <img style="display:block" class="gallery__image" src="${preview}" alt="${description}" />
+          </a>
+        </li>`
+    )
+    .join('');
+
+const inputGalleryToDocument = (() => {
+  document.querySelector('.gallery').innerHTML = createGallery(galleryItems);
+})();
+
+const gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
